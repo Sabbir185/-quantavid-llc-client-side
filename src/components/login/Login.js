77494b6d3172fc38b5074e0c2204d0656a7832a4
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-09-23 13:26:57 +0600

    login and sign up UI implimented

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
+
+    // handle user sign up
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const data = {
+            username: event.target[0].value,
+            password: event.target[1].value,
+        }
+        console.log(data)
+    }
+
     return (
-        <div>
+         <div className="form-container">
+            <form  className="mx-5" onSubmit={handleLogin}>
+                <input type="email" name="email" placeholder="email" className="form-control"/>
+
+                <input type="text" name="password" placeholder="Password" className="form-control mt-2"/>
+
+                <button type="submit" className="btn btn-success text-center mt-4 form-control"> Login </button>
+
+            </form>
+
             
+            <small className="mt-3 d-inline-block">Don't have an account ? please <Link to='/sign-up' className="text-decoration-none">sign up</Link></small>
         </div>
     );
 };
--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './SignUp.css'
 
 const SignUp = () => {
+    // handle user sign up
+    const handleUserSignUp = (event) => {
+        event.preventDefault();
+        const data = {
+            name: event.target[0].value,
+            email: event.target[1].value,
+            password: event.target[2].value,
+        }
+        console.log(data)
+    }
+
+
     return (
         <div>
-            
+             <div className="form-container">
+                    <form  className="mx-5" onSubmit={handleUserSignUp}>
+                       <input type="text" name="name" placeholder="Name" className="form-control mt-2"/>
+
+                       <input type="email" name="email" placeholder="Email" className="form-control mt-2"/>
+
+                       <input type="password" name="password" placeholder="Password" className="form-control mt-2"/>
+
+                       <button type="submit" className="btn btn-success text-center mt-4 form-control"> Sign Up </button>
+
+                    </form>
+
+                    <small className="mt-3 d-inline-block">Already sign up ? please <Link to='/login' className="text-decoration-none">login</Link></small>
+                </div>
         </div>
     );
 };
